b37942b238587403a7b3bcf75d013f3c134a018c
thahir <abuthahircoorg07@gmail.com>
2023-11-13 11:51:59 +0530

    updated

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -8,12 +8,25 @@ window.addEventListener('scroll',()=>{
 
 })
 function Navbar() {
+  var dropdown=true
   return (
     <div className="row">
     <div className="col-12">
     <div className='nav' id='nav'>
     <img src="https://upload.wikimedia.org/wikipedia/commons/0/08/Netflix_2015_logo.svg" className='logo' alt="Netflix" />
     <img src="https://i.pinimg.com/originals/0d/dc/ca/0ddccae723d85a703b798a5e682c23c1.png" className='avtar' alt="Avthar" />  
+    <img src="dropdown.png" className='dropdown'alt='drpdown' onClick={(e)=>{
+      if(dropdown){
+        e.target.style.transform='rotate(180deg)' 
+        dropdown=false
+      }
+      else{
+        e.target.style.transform='rotate(0deg)' 
+        dropdown=true
+      }
+        
+
+    }} />  
     </div>
     </div>
     </div>
